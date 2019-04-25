1cb442f0d04360ee059e84310562b86b30c3b575
Tomas <tomasdaniel09@hotmail.com>
2019-04-24 23:14:51 -0400

    Initial Commit

---
+++ yeet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  yeet
+//
+//  Created by Tomas Daniel Nieto on 2019-04-24.
+//  Copyright © 2019 Tomas Daniel Nieto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
