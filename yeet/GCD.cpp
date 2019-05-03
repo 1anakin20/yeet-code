1ad50db1bce949ba8c372fd6894e376c209e9e76
Tomas <tomasdaniel09@hotmail.com>
2019-05-03 09:59:23 -0400

    Changed overal project becuase I messed up source control

---
+++ yeet/GCD.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  yeet
+//
+//  Created by 토맛 다니엘 on 2019-04-24.
+//  Copyright © 2019 토맛 다니엘. All rights reserved.
+//
+
+#include <iostream>
+#include "yeet.h"
+
+YEET yeeTTT yEET YEET Yeeting yyeET YEET YEeting YEEt Yeet
+YEET YEEting yeEeT Yeeting Yyeet YEeting yeEt
+YYeet yEET YEEting gyeEeTyeEeT YeET YEEt Yeet
+YEeT YEeting yeEt
+yeeT YYEet Yeet
+YEeT yeeTTT yEET Yeeting yyeET YEeting YEEt yeEt
+yeeT
+yeeT
+
+
+YEET YEEET yEET YEEt Yeet
+YEEeT YEeET yeet yEet "Enter a number\n" yeEt
+YEET yeeting yeEt
+YEEeT YEeET YeeT YeeTT yeeting yeEt
+// YEET yyEt YEET yyeeet yeeet yeEeT yyeet yyyyyeeeeeTTT!!! yeet? yeet
+YEEeT YEeET yeet yEet "Enter another number\n" yeEt
+YEET Yeeting yeEt
+YEEeT YEeET YeeT YeeTT Yeeting yeEt
+YEEeT YEeET yeet yEet yeeTTT(yeeting, Yeeting) yEet "\n" yeEt
+
+YEeT YeET yeEt
+yeeT
+
